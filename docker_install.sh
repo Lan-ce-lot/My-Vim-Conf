5b5b93eb47820510680dc0b1cd0b23776ebe43cc
Lancelot <1984737645@qq.com>
2022-11-16 16:27:50 +0800

    refactor: docker install depercation
    Signed-off-by: Lancelot <1984737645@qq.com>

--- docker_install.sh
+++ docker_install.sh
@@ -1,31 +1,43 @@
-# step 1: 安装必要的一些系统工具
-apt-get update
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+# !/bin/bash
+################################################################################################
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
+################################################################################################
+# First, update your existing list of packages 
+sudo apt update
 
-# step 2: 安装GPG证书
-curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+# Tnstall a few prerequisite packages which let apt use packages over HTTPS
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-# Step 3: 写入软件源信息
-add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Add the GPG key for the official Docker repository to your system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Step 4: 更新并安装Docker-CE
-apt-get -y update
-apt-get -y install docker-ce
+# Add the Docker repository to APT sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Step 5: 添加一个配置文件
+# Update your existing list of packages again for the addition to be recognized
+sudo apt update
+
+# Make sure you are about to install from the Docker repo instead of the default Ubuntu repo
+apt-cache policy docker-ce
+
+# Finally, install Docker
+sudo apt install -y docker-ce
+
 #Docker 在默认情况下使用Vgroup Driver为cgroupfs，而Kubernetes推荐使用systemd来替代cgroupfs
 mkdir -p /etc/docker
 cat <<EOF> /etc/docker/daemon.json
 {
     "exec-opts": ["native.cgroupdriver=systemd"],
     "registry-mirrors":[
-        "http://hub-mirror.c.163.com",
+        "https://hub-mirror.c.163.com",
         "https://registry.docker-cn.com",
         "https://docker.mirrors.ustc.edu.cn"
     ]
 }
 EOF
 
-# Step 6: 启动dokcer
-systemctl restart docker
-systemctl enable docker
+# Docker should now be installed, the daemon started, and the process enabled to start on boot. Check that it’s running
+sudo systemctl status docker
+
+
+
